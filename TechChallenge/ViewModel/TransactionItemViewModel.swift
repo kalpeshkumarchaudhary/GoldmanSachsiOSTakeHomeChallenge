8fed9a008754f0de20bf720d2fc374ad685e747b
Kalpesh Chaudhary <kalpesh.chaudhary@xtodoapp.com>
2021-11-22 13:49:26 -0800

    -- Adding TransactionItemViewModel

---
+++ TechChallenge/ViewModel/TransactionItemViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  TransactionItemViewModel.swift
+//  TechChallenge
+//
+//  Created by Kalpesh Chaudhary on 11/22/21.
+//
+
+import Foundation
+
+struct TransactionItemViewModel {
+    let transaction: TransactionModel
+}
