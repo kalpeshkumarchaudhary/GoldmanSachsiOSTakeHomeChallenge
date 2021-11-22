5af25f7b9e34c7c515fc1e29fdd3f658642bb41f
Kalpesh Chaudhary <kalpesh.chaudhary@xtodoapp.com>
2021-11-22 13:47:43 -0800

    -- Adding a TransactionsViewModel

---
+++ TechChallenge/ViewModel/TransactionsViewModel.swift
@@ -0,0 +1,11 @@
+//
+//  TransactionsViewModel.swift
+//  TechChallenge
+//
+//  Created by Kalpesh Chaudhary on 11/22/21.
+//
+
+import Foundation
+
+class TransactionsViewModel: ObservableObject {
+}
