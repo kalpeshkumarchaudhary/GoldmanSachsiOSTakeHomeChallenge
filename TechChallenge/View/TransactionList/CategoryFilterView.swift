069c592868f0e068cf21173c523a145a5372fc1d
Kalpesh Chaudhary <kalpesh.chaudhary@xtodoapp.com>
2021-11-22 12:36:09 -0800

    -- Added files for CategoryFilterView and it's ViewModel

---
+++ TechChallenge/View/TransactionList/CategoryFilterView.swift
@@ -0,0 +1,20 @@
+//
+//  CategoryFilterView.swift
+//  TechChallenge
+//
+//  Created by Kalpesh Chaudhary on 11/22/21.
+//
+
+import SwiftUI
+
+struct CategoryFilterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CategoryFilterView_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryFilterView()
+    }
+}
---
+++ TechChallenge/ViewModel/CategoryFilterViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  CategoryFilterViewModel.swift
+//  TechChallenge
+//
+//  Created by Kalpesh Chaudhary on 11/22/21.
+//
+
+import Foundation
