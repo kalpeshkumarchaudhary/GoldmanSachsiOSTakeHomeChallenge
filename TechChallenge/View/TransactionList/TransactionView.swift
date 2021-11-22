82577fba856b197ab70771633af3d5524d5fc445
Kalpesh Chaudhary <kalpesh.chaudhary@xtodoapp.com>
2021-11-22 14:20:16 -0800

    -- Adding an Identifiable conformance to TransactionItemViewModel -- Adding TransactionItemViewModel into TransactionView -- Updating TransactionView PreviewProvider to use the view model

--- TechChallenge/View/TransactionList/TransactionView.swift
+++ TechChallenge/View/TransactionList/TransactionView.swift
@@ -8,43 +8,44 @@
 import SwiftUI
 
 struct TransactionView: View {
-    let transaction: TransactionModel
+    @Binding var viewModel: TransactionItemViewModel
     
     var body: some View {
         VStack {
-            HStack {
-                Text(transaction.category.rawValue)
-                    .font(.headline)
-                    .foregroundColor(transaction.category.color)
-                Spacer()
-            }
-            
-            HStack {
-                transaction.image
-                    .resizable()
-                    .frame(
-                        width: 60.0,
-                        height: 60.0,
-                        alignment: .top
-                    )
-                
-                VStack(alignment: .leading) {
-                    Text(transaction.name)
-                        .secondary()
-                    Text(transaction.accountName)
-                        .tertiary()
-                }
-                
-                Spacer()
-                
-                VStack(alignment: .trailing) {
-                    Text("$\(transaction.amount.formatted())")
-                        .bold()
-                        .secondary()
-                    Text(transaction.date.formatted)
-                        .tertiary()
-                }
-            }
+//            HStack {
+//                Text(transaction.category.rawValue)
+//                    .font(.headline)
+//                    .foregroundColor(transaction.category.color)
+//                Spacer()
+//            }
+//
+//            HStack {
+//                transaction.image
+//                    .resizable()
+//                    .frame(
+//                        width: 60.0,
+//                        height: 60.0,
+//                        alignment: .top
+//                    )
+//
+//                VStack(alignment: .leading) {
+//                    Text(transaction.name)
+//                        .secondary()
+//                    Text(transaction.accountName)
+//                        .tertiary()
+//                }
+//
+//                Spacer()
+//
+//                VStack(alignment: .trailing) {
+//                    Text("$\(transaction.amount.formatted())")
+//                        .bold()
+//                        .secondary()
+//                    Text(transaction.date.formatted)
+//                        .tertiary()
+//                }
+//            }
+            Text("Place Holder")
         }
         .padding(8.0)
         .background(Color.accentColor.opacity(0.1))
@@ -56,8 +57,8 @@ struct TransactionView: View {
 struct TransactionView_Previews: PreviewProvider {
     static var previews: some View {
         VStack {
-            TransactionView(transaction: ModelData.sampleTransactions[0])
-            TransactionView(transaction: ModelData.sampleTransactions[1])
+            TransactionView(viewModel: .constant(TransactionItemViewModel(transaction: ModelData.sampleTransactions[0])))
+            TransactionView(viewModel: .constant(TransactionItemViewModel(transaction: ModelData.sampleTransactions[1])))
         }
         .padding()
         .previewLayout(.sizeThatFits)
--- TechChallenge/ViewModel/TransactionItemViewModel.swift
+++ TechChallenge/ViewModel/TransactionItemViewModel.swift
@@ -10,3 +10,9 @@ import Foundation
 struct TransactionItemViewModel {
     let transaction: TransactionModel
 }
+
+extension TransactionItemViewModel: Identifiable {
+    var id: Int {
+        return transaction.id
+    }
+}
